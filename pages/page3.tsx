522f86ee8e1eaa72a244e9f02210750c55a18de6
VinRicci <vinicioricci@gmail.com>
2023-04-25 21:11:46 -0600

    Avance en proyectos

--- pages/page3.tsx
+++ pages/page3.tsx
@@ -1,4 +1,6 @@
 import styles from '/styles/page3.module.css'
+import Image from 'next/image'
+import img from '/images/perfil.png'
 
 export default function page3 () {
     return(
@@ -18,32 +20,32 @@ export default function page3 () {
         </nav>
             <div className={styles.contenedor}>
                 <div className={styles.container}>
-                    {/* imagen */}
+                    <Image src={img} alt="Imagen1" className={styles.itemp} />
                     <div className={styles.info}>
-                        <h2>Titulo</h2>
+                        <h2>Proyecto de asistencias en URL</h2>
                         <p>Descripcion</p>
                     </div>
                 </div>
                 <div className={styles.container}>
-                    {/* imagen */}
+                    <Image src={img} alt="Imagen1" className={styles.itemp} />
                     <div className={styles.info}>
-                        <h2>Titulo</h2>
+                        <h2>Proyecto de alcancia</h2>
                         <p>Descripcion</p>
                     </div>
                 </div>
                 <div className={styles.container}>
-                    {/* imagen */}
+                    <Image src={img} alt="Imagen1" className={styles.itemp} />
                     <div className={styles.info}>
-                        <h2>Titulo</h2>
+                        <h2>Proyecto de Active Directory</h2>
                         <p>Descripcion</p>
                     </div>
                 </div>
             </div>
-            <div className={styles.shapebottom}>
+            {/* <div className={styles.shapebottom}>
                 <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
                     <path d="M1200 120L0 16.48 0 0 1200 0 1200 120z" className={styles.shapefillbottom}></path>
                 </svg>
-            </div>
+            </div> */}
         </div>
         </>
     )
