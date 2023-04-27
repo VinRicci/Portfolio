81be7e663afe338553bf140781b2685565a85b5f
VinRicci <vinicioricci@gmail.com>
2023-04-27 17:05:14 -0600

    Blog creado

--- pages/page2.tsx
+++ pages/page2.tsx
@@ -1,11 +1,50 @@
 import styles from '/styles/page2.module.css'
 import Image from 'next/image'
+import axios from 'axios';
 import img from '/images/perfil.png'
 import Link from 'next/link'
+import { useEffect, useState } from 'react';
+import { GetServerSideProps } from 'next';
 
-export default function page2 () {
+interface Post {
+    id: number;
+    attributes: {
+        Title: string;
+        Content: string;
+        createdAt: string;
+        updatedAt: string;
+        publishedAt: string;
+    };
+}
+
+interface StrapiResponse {
+    data: Post[];
+}
+interface Props {
+    posts: Post[];
+}
+
+export const getServerSideProps: GetServerSideProps<Props> = async () => {
+    const endpoint = `https://strapi-production-ff73.up.railway.app/api/blogs`;
+    const response = await axios.get<StrapiResponse>(endpoint);
+    const posts = response.data.data;
+    return {
+    props: {
+        posts,
+    },
+    };
+};
+
+export default function page2 (props: Props) {
+    const { posts } = props;
     return (
         <>
+        {/* <section>
+            <h1>Posts</h1>
+            {posts.map(post => (
+                <p key={post.id}>{post.attributes.Title}</p>
+            ))}
+        </section> */}
         <div className={styles.page}>
             <div className={styles.shapetop}>
                 <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
@@ -22,8 +61,14 @@ export default function page2 () {
             <div className={styles.container}>
                 <Image src={img} alt="Imagen1" className={styles.itemp} />
                 <div className={styles.texto}>
-                    <h1>Titulo</h1>
-                    <p>Descripcion</p>
+                    {posts.map(post => (
+                        <h1 key={post.id}>{post.attributes.Title}</h1>
+                    ))}
+                    {/* <h1>Titulo</h1> */}
+                    {posts.map(post => (
+                        <p key={post.id}>{post.attributes.Content}</p>
+                    ))}
+                    {/* <p>Descripcion</p> */}
                 </div>
             </div>
             <div className={styles.container}>
